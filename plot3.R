04dda4d70aaa974e71b48c69be905fafd9c4b21c
Lee Amenya <loamenya@netscape.net>
2015-05-10 14:58:43 -0700

    added assignment 1 to repo

---
+++ plot3.R
@@ -0,0 +1,46 @@
+#read the whole dataset
+download_url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!file.exists("household_power_consumption.txt")){
+  download.file(download_url, destfile ="household_power_consumption.zip" ,mode="wb")
+  unzip("household_power_consumption.zip")
+} 
+data <- read.csv2(file = "household_power_consumption.txt",na.strings="?")
+message(sprintf("Read %s rows", nrow(data)))
+
+message(sprintf("Date column is of class: %s", class(data$Date)))
+#convert the Date from factor to date
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+message(sprintf("Converted Date column to class: %s", class(data$Date)))
+
+
+#filter the dataset to dates
+data_subset <- subset(data, Date >= '2007-02-01' & Date <= '2007-02-02')
+message(sprintf("Filtered data by date to %s rows", nrow(data_subset)))
+
+#create a date and time column
+data_subset$Date_Time <- paste(data_subset$Date,data_subset$Time)
+message(sprintf("Created Date_Time column of class: %s", class(data_subset$Date_Time)))
+
+#convert Date_Time to Posixlt
+data_subset$Date_Time <- strptime(data_subset$Date_Time, format="%Y-%m-%d %H:%M:%S")
+message(sprintf("Converted Date_Time column to class: %s", class(data_subset$Date_Time)))
+
+message(sprintf("Global_active_power column is of class: %s", class(data$Global_active_power)))
+data_subset$Global_active_power <- suppressWarnings(as.double(data_subset$Global_active_power))
+message(sprintf("Converted Global_active_power column to class: %s", class(data_subset$Global_active_power)))
+
+#exclude missing data
+data_subset <- subset(data_subset,!is.na(Global_active_power))
+graph_data1<- data_subset[,c("Date_Time","Sub_metering_1")]
+graph_data2<- data_subset[,c("Date_Time","Sub_metering_2")]
+graph_data3<- data_subset[,c("Date_Time","Sub_metering_3")]
+
+
+#plot to png device
+png(filename="plot3.png",width = 504,height = 504,units ="px", bg="white")
+plot(graph_data1,pch =".",type ="l",ylab="Energy sub metering", xlab="" )
+lines(graph_data2,pch =".",type ="l",col="red")
+lines(graph_data3,pch =".",type ="l",col="blue")
+legend("topright", lty = c(1, 1), lwd = c(1, 1, 1), col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
